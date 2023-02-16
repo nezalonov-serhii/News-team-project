fc2ae7f70d00de15e88eeb732abbaccd67a15723
nezalonov-serhii <110600342+nezalonov-serhii@users.noreply.github.com>
2023-02-16 21:51:21 +0000

    Deploying to gh-pages from @ nezalonov-serhii/News-team-project@189fc8c1bf16c4dddbff0b60cc8e8d3fb5694a97 🚀

---
+++ index.2eb8eaad.js
@@ -0,0 +1,2 @@
+const o={body:document.querySelector("body")};console.log(o.body);
+//# sourceMappingURL=index.2eb8eaad.js.map
---
+++ index.6730db8e.js
@@ -0,0 +1,2 @@
+!function(){var o={body:document.querySelector("body")};console.log(o.body)}();
+//# sourceMappingURL=index.6730db8e.js.map
