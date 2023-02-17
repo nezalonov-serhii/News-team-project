34b7cd30f2afae6657f54219ca92d5effee4ba6b
Sergii <nezalonov.serhii@gmail.com>
2023-02-17 16:24:39 +0200

    add fetch weather

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -1,2 +1,12 @@
 const WEATHER_KEY = 'adbfcea561ee5005b6167772340c12ff';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
+async function getWeather(location) {
+  const weatherFetch = await fetch(
+    `${BASE_URL}?units=metric&q=${location}&appid=${WEATHER_KEY}`
+  );
+
+  return weatherFetch.json();
+}
+
+export { getWeather };
