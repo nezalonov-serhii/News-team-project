6060a96e1238574da31798e4ab56f78b3f32b296
Sergii <nezalonov.serhii@gmail.com>
2023-02-17 12:19:06 +0200

    add svg sprite, key to API and color var

---
+++ src/js/api/news.js
@@ -0,0 +1,2 @@
+const KEY = 'api-key=ccA9QsXbvrHhGuvowEcHjyxEU2jAukPO';
+const BASE_URL = 'https://api.nytimes.com/svc';
---
+++ src/js/api/weather.js
@@ -0,0 +1,2 @@
+const WEATHER_KEY = 'adbfcea561ee5005b6167772340c12ff';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
