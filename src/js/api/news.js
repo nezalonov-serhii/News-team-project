dc500a6247417765fd28fd17e1cf254ce056a8e4
Sergii <nezalonov.serhii@gmail.com>
2023-02-18 10:42:39 +0200

    Update news.js

--- src/js/api/news.js
+++ src/js/api/news.js
@@ -1,64 +1,63 @@
-
-const KEY = "api-key=ccA9QsXbvrHhGuvowEcHjyxEU2jAukPO";
-const BASE_URL = "https://api.nytimes.com/svc";
+const KEY = 'api-key=ccA9QsXbvrHhGuvowEcHjyxEU2jAukPO';
+const BASE_URL = 'https://api.nytimes.com/svc';
 
 async function getPopular() {
-   const articleFetch = await fetch(
-      `${BASE_URL}/mostpopular/v2/viewed/1.json?${KEY}`
-   );
+  const articleFetch = await fetch(
+    `${BASE_URL}/mostpopular/v2/viewed/1.json?${KEY}`
+  );
 
-   if (articleFetch.ok) {
-      const popular = await articleFetch.json();
-      return popular.results;
-   }
-   throw new Error(articleFetch.statusText);
+  if (articleFetch.ok) {
+    const popular = await articleFetch.json();
+    return popular.results;
+  }
+  throw new Error(articleFetch.statusText);
 }
 
 async function getCategoryList() {
-   const categoryList = await fetch(
-      `${BASE_URL}//news/v3/content/section-list.json?${KEY}`
-   );
+  const categoryList = await fetch(
+    `${BASE_URL}//news/v3/content/section-list.json?${KEY}`
+  );
 
-   if (categoryList.ok) {
-      const categories = await categoryList.json();
-      return categories.results;
-   }
+  if (categoryList.ok) {
+    const categories = await categoryList.json();
+    return categories.results;
+  }
 
-   throw new Error(categoryList.statusText);
+  throw new Error(categoryList.statusText);
 }
 
 async function getDataByCategory(value) {
-   let normalazeValue = value.toLowerCase();
-   let newValue = encodeURIComponent(normalazeValue);
+  let normalazeValue = value.toLowerCase();
+  let newValue = encodeURIComponent(normalazeValue);
 
-   const data = await fetch(
-      `${BASE_URL}/news/v3/content/all/${newValue}.json?${KEY}`
-   );
-   if (data.ok) {
-      const articles = await data.json();
+  const data = await fetch(
+    `${BASE_URL}/news/v3/content/all/${newValue}.json?${KEY}`
+  );
+  if (data.ok) {
+    const articles = await data.json();
 
-      return articles.results;
-   }
+    return articles.results;
+  }
 
-   throw new Error(data.statusText);
+  throw new Error(data.statusText);
 }
 
 async function getSearchArticle(value, page) {
-   let dateForUrl = "";
+  let dateForUrl = '';
+  // dateForUrl = ` &begin_date=${date}&end_date=${date}`;
+  // &page=26
+  const articleFetch = await fetch(
+    `${BASE_URL}/search/v2/articlesearch.json?q=${value}&page=${page}&${KEY}${dateForUrl}`
+  );
 
-   const articleFetch = await fetch(
-      `${BASE_URL}/search/v2/articlesearch.json?q=${value}&page=${page}&${KEY}${dateForUrl}`
-   );
+  https: if (articleFetch.ok) {
+    const articles = await articleFetch.json();
 
-   if (articleFetch.ok) {
-      const articles = await articleFetch.json();
+    console.log(articles.response);
+    return articles.response.docs;
+  }
 
-      console.log(articles.response);
-      return articles.response.docs;
-   }
-
-   throw new Error(articleFetch.statusText);
+  throw new Error(articleFetch.statusText);
 }
 
 export { getPopular, getCategoryList, getSearchArticle, getDataByCategory };
-
