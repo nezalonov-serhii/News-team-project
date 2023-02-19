3aef01781d897ca18fa4cd15eb4fda17b9a7415a
Viktoria-Korol <vkqween@gmail.com>
2023-02-19 16:21:59 +0200

    finish header

---
+++ src/src/index.js
@@ -0,0 +1,11 @@
+import './js/mobileMenu/mobileMenu';
+
+import './js/popularRender/popularRender';
+import './js/newsFilter/fillter';
+import './js/weather/weather';
+
+import './js/search/search';
+import './js/calendar/calendar';
+import './js/pagination/pagination';
+
+import './js/darkMode/darkMode';
---
+++ src/src/js/darkMode/darkMode.js
@@ -0,0 +1,3 @@
+import { refs } from '../refs/refs';
+
+// console.log(refs.body);
