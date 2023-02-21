02199f8ff9c7a5816173b6a77db4288dfce39752
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-02-21 22:24:55 +0200

    Add widget to mainPage

--- src/js/markup/markup.js
+++ src/js/markup/markup.js
@@ -7,7 +7,8 @@ import {
   getDataByCategory,
 } from '../api/news';
 import { createNewsCard, newsCardTextFormat } from '../newsCard/newsCard';
-
+import { fillWeather } from '../weather/weather';
+// fillWeather()
 let arrayNewsCard = [];
 const error =
   'https://img.freepik.com/free-vector/404-error-with-cute-animal-concept-illustration_114360-1931.jpg';
@@ -16,6 +17,8 @@ let orderedNumber = 0;
 
 refs.form.addEventListener('submit', renderSearchNews);
 refs.filterCategories.addEventListener('click', renderNewsCategory);
+
+
 
 function saveValuesFromCategoryNews(articles) {
   console.log(articles);
@@ -41,7 +44,8 @@ function saveValuesFromSearchNews(articles) {
       title: article.headline.main,
 
       media: `${
-        article.multimedia[0] === undefined
+        // article.multimedia[0] === undefined 
+        article.multimedia.length === 0
           ? error
           : `https://static01.nyt.com/${article.multimedia[0].url}`
       }`,
@@ -129,10 +133,10 @@ function renderNewsCategory(e) {
 function renderNewsList(arrayNewsCard) {
   const markup = arrayNewsCard.reduce((previousValue, article, index) => {
     orderedNumber += 1;
+    
     if (index === 2) {
-      createMarkupWidgetWeather(orderedNumber) + previousValue;
-    }
-
+      return   createMarkupWidgetWeather() + previousValue;
+     }
     return createNewsCard(article, orderedNumber) + previousValue;
   }, '');
   updateNewsList(markup);
@@ -140,12 +144,43 @@ function renderNewsList(arrayNewsCard) {
 }
 
 function updateNewsList(markup) {
-  // refs.newsList.insertAdjacentHTML('beforeend', markup);
+  
   refs.newsList.innerHTML = markup;
+const deg = document.querySelector('.weather__degree');
+const value = document.querySelector('.weather__value');
+const city = document.querySelector('.weather__city');
+const day = document.querySelector('.weather__day');
+const year = document.querySelector('.weather__year');
+const imgWeather = document.querySelector('.weather__image');
+fillWeather(deg,value,city,day,year,imgWeather)
+
 }
 function createMarkupWidgetWeather() {
-  return `<li class =" news__item location_weather"  ><div class=" news__weather"><p class = "text_weather">Weather<p></div></li>`;
+ 
+
+ return `<li id="weather" class="weather news__item location_weather">
+<div class="weather__position">
+    <span class="weather__degree"></span>
+    <div class="weather__item">
+      <span class="weather__value"></span>
+      <p class="weather__location">
+        <svg>
+          <use href="./images/sprite.svg#location"></use>
+        </svg>
+        <span class="weather__city"></span>
+      </p>
+    </div>
+  </div>
+  <img class="weather__image" />
+  <div class="weather__date">
+    <p class="weather__day"></p>
+    <p class="weather__year"></p>
+  </div>
+  <a href="https://sinoptik.ua/" class="weather__link" target="_blank" rel="noreferrer noopener">weather for week</a>
+</li>`
+
 }
+
 export {
   renderNewsList,
   updateNewList,
--- src/js/weather/weather.js
+++ src/js/weather/weather.js
@@ -1,13 +1,14 @@
 import { getWeather } from '../api/weather';
+import{refs} from '../refs/refs'
+// const deg = document.querySelector('.weather__degree');
+// const value = document.querySelector('.weather__value');
+// const city = document.querySelector('.weather__city');
+// const day = document.querySelector('.weather__day');
+// const year = document.querySelector('.weather__year');
+// const imgWeather = document.querySelector('.weather__image');
 
-const deg = document.querySelector('.weather__degree');
-const value = document.querySelector('.weather__value');
-const city = document.querySelector('.weather__city');
-const day = document.querySelector('.weather__day');
-const year = document.querySelector('.weather__year');
-const imgWeather = document.querySelector('.weather__image');
-
-const fillWeather = async () => {
+const fillWeather = async (deg,value,city,day,year,imgWeather) => {
+  console.log(deg,value,city,day,year,imgWeather)
   const currentWeather = await getWeather('London');
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const months = [
@@ -35,5 +36,38 @@ const fillWeather = async () => {
   imgWeather.src = `http://openweathermap.org/img/wn/${currentWeather.weather.icon}@4x.png`;
   console.log(currentWeather);
 };
+// function fillWeather() {
+//   console.log(refs.deg)
+//   const currentWeather = getWeather('London');
+//   console.log(currentWeather.temp)
+//   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//   const months = [
+//     'Jan',
+//     'Feb',
+//     'Mar',
+//     'Apr',
+//     'May',
+//     'Jun',
+//     'Jul',
+//     'Aug',
+//     'Sep',
+//     'Oct',
+//     'Nov',
+//     'Dec',
+//   ];
+//   const date = new Date();
+//   refs.deg.innerText = `${Math.round(currentWeather.temp)}°`;
+//   refs.city.innerText = currentWeather.location;
+//   refs.day.innerText = days[date.getDay()];
+//   refs.year.innerText = `${date.getDate()} ${
+//     months[date.getMonth()]
+//   } ${date.getFullYear()}`;
+//   refs.value.innerText = currentWeather.weather.type;
+//   refs.imgWeather.src = `http://openweathermap.org/img/wn/${currentWeather.weather.icon}@4x.png`;
+//   console.log(currentWeather);
+// };
 
-fillWeather();
+
+
+// fillWeather();
+export{fillWeather}
