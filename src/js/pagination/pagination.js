f93d6ea71400b2f5f7cc62bf44d2836a03c33866
Ivan-coder74 <109940619+Ivan-coder74@users.noreply.github.com>
2023-02-21 04:09:42 +0100

    pagination

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -1,20 +1,98 @@
+// const prevBtn = document.querySelector('#prev-btn');
+// const nextBtn = document.querySelector('#next-btn');
+// // const pageBtns = document.querySelectorAll('.page-btn');
+
+// // поточна сторінка
+// let currentPage = 1;
+
+// // кількість новин на сторінці
+// const newsPerPage = 20;
+
+// // обробка подій на кнопки "Prev" та "Next"
+// prevBtn.addEventListener('click', () => {
+//   currentPage--;
+//   loadNews(currentPage);
+// });
+
+// nextBtn.addEventListener('click', () => {
+//   currentPage++;
+//   loadNews(currentPage);
+// });
+
+import { getPopular, getSearchArticle } from '../api/news';
+const itemsPerPage = window.matchMedia('(max-width: 768px)').matches
+  ? 6
+  : window.matchMedia('(max-width: 1024px)').matches
+  ? 8
+  : 10;
+
+let currentPage = 1;
+function loadNews(data, page) {
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const itemsToShow = data.slice(start, end);
+  // відображення елементів на поточній сторінці
+}
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
-// const pageBtns = document.querySelectorAll('.page-btn');
+prevBtn.disabled = true;
+nextBtn.disabled = false;
+getPopular()
+  .then(data => {
+    prevBtn.addEventListener('click', () => {
+      if (currentPage > 1) {
+        currentPage--;
+        loadNews(data, currentPage, itemsPerPage);
+      }
+      if (currentPage === 1) {
+        prevBtn.disabled = true;
+      }
+      nextBtn.disabled = false;
+    });
+    nextBtn.addEventListener('click', () => {
+      if (currentPage < totalPages) {
+        currentPage++;
+        loadNews(data, currentPage, itemsPerPage);
 
-// поточна сторінка
-let currentPage = 1;
+        if (currentPage === totalPages) {
+          nextBtn.disabled = true;
+        }
+        prevBtn.disabled = false;
+      }
+    });
+    // console.log(data);
+    totalPages = Math.ceil(data.length / itemsPerPage); // визначення загальної кількості сторінок
+    loadNews(data, currentPage, itemsPerPage); // відображення елементів на першій сторінці
+    if (totalPages === 1) {
+      nextBtn.disabled = true;
+    }
+    console.log(data);
+  })
+  .catch(error => console.log(error));
+console.log(itemsPerPage);
 
-// кількість новин на сторінці
-const newsPerPage = 20;
+function loadNews(data, page) {
+  const start = (page - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const itemsToShow = data.slice(start, end);
+}
 
-// обробка подій на кнопки "Prev" та "Next"
-prevBtn.addEventListener('click', () => {
-  currentPage--;
-  loadNews(currentPage);
-});
+//   const newsList = document.getElementById('news-list');
+//   newsList.innerHTML = '';
 
-nextBtn.addEventListener('click', () => {
-  currentPage++;
-  loadNews(currentPage);
-});
+//   itemsToShow.forEach(item => {
+//     const newsItem = document.createElement('div');
+//     newsItem.classList.add('news-item');
+//     newsItem.innerHTML = `
+//       <div class="news-item__image">
+//         <img src="${item.urlToImage}" alt="">
+//       </div>
+//       <div class="news-item__content">
+//         <h2 class="news-item__title">${item.title}</h2>
+//         <p class="news-item__description">${item.description}</p>
+//         <a class="news-item__link" href="${item.url}" target="_blank">Read more</a>
+//       </div>
+//     `;
+//     newsList.appendChild(newsItem);
+//   });
+// }
