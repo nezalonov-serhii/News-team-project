1079a2ec3a3b8c5256392edd023cfcca68ad7627
Sergii <nezalonov.serhii@gmail.com>
2023-02-22 18:35:11 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './js/mobileMenu/mobileMenu';
 import './js/mobileMenu/mobileMenuCurrent';
 
 import './js/markup/markup';
-import './js/popularRender/popularRender';
 
 import './js/newsFilter/filter';
 import './js/newsCard/newsCard';
@@ -11,9 +10,9 @@ import './js/api/weather';
 import './js/weather/weather';
 
 import './js/pagination/search';
-import './js/calendar/calendar';
-import './js/pagination/popular';
 import './js/pagination/fillterPagination';
+import './js/pagination/popular';
+import './js/calendar/calendar';
 
 import './js/loader/loader';
 import './js/darkMode/darkMode';
