42e8cf391db93e65dcdc1bee3283b39ad7812279
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-02-22 22:51:15 +0200

    Add clearFunction

--- src/js/markup/markup.js
+++ src/js/markup/markup.js
@@ -18,7 +18,8 @@ let orderedNumber = 0;
 refs.form.addEventListener('submit', renderSearchNews);
 
 function saveValuesFromCategoryNews(articles) {
-  refs.newsList.innerHTML = '';
+  // refs.newsList.innerHTML = '';
+  clearNewsList()
   console.log(arrayNewsCard);
   arrayNewsCard = [];
 
@@ -79,7 +80,8 @@ function saveValuesFromPopularNews(articles) {
 }
 
 function renderPopularNews(articles) {
-  refs.newsList.innerHTML = '';
+  // refs.newsList.innerHTML = '';
+  clearNewsList()
   arrayNewsCard = [];
 
   saveValuesFromPopularNews(articles);
@@ -89,7 +91,8 @@ function renderPopularNews(articles) {
 function renderSearchNews(e) {
   e.preventDefault();
 
-  refs.newsList.innerHTML = '';
+  // refs.newsList.innerHTML = '';
+  clearNewsList()
   arrayNewsCard = [];
 
   const date = refs.celendarDate.dataset.time.replaceAll('-', '');
@@ -130,31 +133,36 @@ function renderSearchNews(e) {
 //}
 
 function renderNewsList(arrayNewsCard) {
+ 
   const markup = arrayNewsCard.reduce((previousValue, article, index) => {
     orderedNumber += 1;
+    return checkIndex(
+      arrayNewsCard,
+      previousValue,
+      article,
+      index,
+      orderedNumber
+    );
 
-    if (index === 2) {
-      return (
-        createMarkupWidgetWeather() +
-        previousValue +
-        createNewsCard(article, orderedNumber)
-      );
-    }
-    return createNewsCard(article, orderedNumber) + previousValue;
+   
   }, '');
   updateNewsList(markup);
+//  const dataOfWeather = updateNewsList(markup);
+ 
   orderedNumber = 0;
 }
 
+const mainNewsList = refs.newsList
+
 function updateNewsList(markup) {
-  refs.newsList.innerHTML = markup;
-  const deg = document.querySelector('.weather__degree');
-  const value = document.querySelector('.weather__value');
-  const city = document.querySelector('.weather__city');
-  const day = document.querySelector('.weather__day');
-  const year = document.querySelector('.weather__year');
-  const imgWeather = document.querySelector('.weather__image');
-  fillWeather(deg, value, city, day, year, imgWeather);
+  mainNewsList.innerHTML = markup;
+  const deg = mainNewsList.querySelector('.weather__degree');
+  const value = mainNewsList.querySelector('.weather__value');
+  const city = mainNewsList.querySelector('.weather__city');
+  const day = mainNewsList.querySelector('.weather__day');
+  const year =mainNewsList.querySelector('.weather__year');
+  const imgWeather = mainNewsList.querySelector('.weather__image');
+  fillWeather(deg, value, city, day, year, imgWeather)
 }
 function createMarkupWidgetWeather() {
   return `<li id="weather" class="weather news__item location_weather">
@@ -190,4 +198,40 @@ export {
 };
 function normolizeDate(date) {
   return date.slice(0, 10);
+}
+function checkIndex(
+  arrayNewsCard,
+  previousValue,
+  article,
+  index,
+  orderedNumber
+) {
+  // if(arrayNewsCard.length === 0) {
+  //   return
+  // }
+  if (index === 2) {
+    console.log(index);
+
+    return (
+      createMarkupWidgetWeather() +
+      previousValue +
+      createNewsCard(article, orderedNumber)
+    );
+  } else if (arrayNewsCard.length === 2 && index === 1) {
+    return (
+      createMarkupWidgetWeather() +
+      previousValue +
+      createNewsCard(article, orderedNumber)
+    );
+  } else if (arrayNewsCard.length === 1) {
+    return (
+      createMarkupWidgetWeather() +
+      previousValue +
+      createNewsCard(article, orderedNumber)
+    );
+  }
+  return createNewsCard(article, orderedNumber) + previousValue;
+}
+function clearNewsList(){
+  refs.newsList.innerHTML = '';
 }
