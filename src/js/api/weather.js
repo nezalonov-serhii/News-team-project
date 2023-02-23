4355832676a3be267dca68577fd393f42d19d120
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-23 18:13:12 +0200

    update func location

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -1,21 +1,7 @@
 const WEATHER_KEY = 'adbfcea561ee5005b6167772340c12ff';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
-// async function getWeather(location) {
-//   const weatherFetch = await fetch(
-//     `${BASE_URL}units=metric&q=${location}&appid=${WEATHER_KEY}`
-//   );
-//   const weatherInfo = await weatherFetch.json();
 
-//   return {
-//     location: weatherInfo.name,
-//     temp: weatherInfo.main.temp,
-//     weather: {
-//       type: weatherInfo.weather[0].description,
-//       icon: weatherInfo.weather[0].icon,
-//     },
-//   };
-// }
 async function getWeather(latitude, longitude) {
   const weatherFetch = await fetch(
     `${BASE_URL}units=metric&lat=${latitude}&lon=${longitude}&appid=${WEATHER_KEY}`
@@ -31,5 +17,5 @@ async function getWeather(latitude, longitude) {
     },
   };
 }
-//getLocation('London')
+
 export { getWeather };
