3f15e7fa197f59ce3fb0df290a94a3f3d0c86575
Sergii <nezalonov.serhii@gmail.com>
2023-02-24 17:41:07 +0200

    Update popular.js

--- src/js/pagination/popular.js
+++ src/js/pagination/popular.js
@@ -14,9 +14,9 @@ let rightAmount = [];
 if (window.matchMedia('(max-width: 767.98px)').matches) {
   newsPerPage = 4;
 } else if (window.matchMedia('(min-width: 1280px)').matches) {
-  newsPerPage = 8;
-} else if (window.matchMedia('(min-width: 768px)').matches) {
   newsPerPage = 7;
+} else if (window.matchMedia('(min-width: 768px)').matches) {
+  newsPerPage = 8;
 }
 
 getPopular()
